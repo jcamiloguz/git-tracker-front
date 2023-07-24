cf8eb89d839569853d3f49e00c0159de9184b5e8
jcamiloguz <jcamiloguz1@gmail.com>
2023-07-24 08:21:14 -0500

    feat: :sparkles: Add react-query and axios clients

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Toaster } from 'react-hot-toast'
 import { Hero } from './components/Hero'
 import { Tracker } from './components/Tracker'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <main className="mx-auto min-h-screen w-full max-w-[768px] ">
-      <Hero
-        Title="Git Tracker!"
-        description="Git tracker is a user-friendly app that displays the git commit history of this current project. It encourages well-defined commits and regular updates, promoting clarity and seamless collaboration within your team."
-      />
-      <Tracker />
-      <Toaster position="bottom-center" />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="mx-auto min-h-screen w-full max-w-[768px] ">
+        <Hero
+          Title="Git Tracker!"
+          description="Git tracker is a user-friendly app that displays the git commit history of this current project. It encourages well-defined commits and regular updates, promoting clarity and seamless collaboration within your team."
+        />
+        <Tracker />
+        <Toaster position="bottom-center" />
+      </main>
+    </QueryClientProvider>
   )
 }
 
