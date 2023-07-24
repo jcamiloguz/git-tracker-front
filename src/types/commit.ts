4834333c4b14857de3792b1b9d8f30e6efbeadb1
jcamiloguz <jcamiloguz1@gmail.com>
2023-07-24 08:15:55 -0500

    feat: :label: Add repo and commit types

---
+++ src/types/commit.ts
@@ -0,0 +1,51 @@
+export interface ICommit {
+  sha: string
+  node_id: string
+  commit: ICommitClass
+  url: string
+  html_url: string
+  comments_url: string
+  author: IAuthor
+  committer: IAuthor
+}
+
+export interface IAuthor {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+}
+
+export interface ICommitClass {
+  message: string
+  tree: ITree
+  url: string
+  comment_count: number
+  verification: IVerification
+}
+
+export interface ITree {
+  sha: string
+  url: string
+}
+
+export interface IVerification {
+  verified: boolean
+  reason: string
+  signature: null
+  payload: null
+}
